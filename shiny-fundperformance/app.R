4bdfd6aaa8c70eddd5c5adcbe64d1ec59cc9bb14
Justin <justinyeo@u.nus.edu>
2022-06-02 11:56:22 +0800

    update app.R

--- shiny-fundperformance/app.R
+++ shiny-fundperformance/app.R
@@ -3,6 +3,8 @@ library(highcharter)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(pivottabler)
+library(shinyWidgets)
 
 # setwd("C:\\Users\\yeoshuiming\\Downloads\\Assignments")
 
@@ -20,72 +22,94 @@ years = unique(perf_data$Date)
 
 # UI  ---------------------------------------------------------
 ui <-
-  fluidPage(# App title -------------------------------------------------------
-            titlePanel(h2(
-              "Risk Analysis Dashboard",
-              align = "center",
-              style = list(fontWeight = "bold")
-            )),
-            
-            # Sidebar layout with a input and output definitions --------------
-            sidebarLayout(
-              # Inputs --------------------------------------------------------
-              sidebarPanel(
-                sliderInput(
-                  inputId = "dateRange",
-                  label = "Month End Dates",
-                  min = min(perf_data$Date),
-                  max = max(perf_data$Date),
-                  step = 1,
-                  value = c(max(perf_data$Date) - days(30 * 15), max(perf_data$Date)),
-                  timeFormat = "%Y-%m-%d"
-                ),
-                
-                selectInput(
-                  inputId = "plotType",
-                  label = "Plot Type:",
-                  choices = c(
-                    "Area" = "area",
-                    "Bar" = "column",
-                    "Line" = "line"
-                  ),
-                  selected = "column"
-                ),
-                
-                selectInput(
-                  inputId = "measureSelect",
-                  label = "Risk measure:",
-                  choices = choiceMeasure,
-                  selected = "TotalReturn"
-                ),
-                
-                selectInput(
-                  inputId = "lookbackPeriod",
-                  label = "Lookback Period:",
-                  choices = choiceLookback,
-                  selected = "MTD"
-                ),
-                
-                selectInput(
-                  inputId = "theme",
-                  label = "Theme",
-                  choices = c(
-                    "No theme",
-                    "Chalk" = "chalk",
-                    "Dark Unica" = "darkunica",
-                    "Economist" = "economist",
-                    "FiveThirtyEight" = "fivethirtyeight",
-                    "Gridlight" = "gridlight",
-                    "Handdrawn" = "handdrawn",
-                    "Sandsignika" = "sandsignika"
-                  )
-                )
-              ),
-              
-              # Output --------------------------------------------------------
-              mainPanel(highchartOutput("hcontainer", height = "500px"))
-              
-            ))
+  fluidPage(
+    # App title -------------------------------------------------------
+    titlePanel(h2(
+      "Risk Analysis Dashboard",
+      align = "center",
+      style = list(fontWeight = "bold")
+    )),
+    
+    # Output --------------------------------------------------------
+    mainPanel(
+      "Copyrights(c)2022 XIX",
+      fluidRow(highchartOutput(
+        "hcontainer", height = "550px", width = "100%"
+      )),
+      fluidRow(pivottablerOutput(
+        "pvt", height = "550px", width = "100%"
+      ))
+    ),
+    hr(),
+    
+    # Bottom row layout with a input and output definitions --------------
+    sidebarPanel(
+      h4("Chart Parameters:"),
+      sliderInput(
+        inputId = "dateRange",
+        label = "Month End Dates",
+        min = min(perf_data$Date),
+        max = max(perf_data$Date),
+        step = 1,
+        value = c(max(perf_data$Date) - days(30 * 15), max(perf_data$Date)),
+        timeFormat = "%Y-%m-%d",
+        width = "100%"
+      ),
+      selectInput(
+        inputId = "plotType",
+        label = "Plot Type:",
+        choices = c(
+          "Area" = "area",
+          "Bar" = "column",
+          "Line" = "line"
+        ),
+        selected = "column"
+      ),
+      selectInput(
+        inputId = "measureSelect",
+        label = "Risk measure:",
+        choices = choiceMeasure,
+        selected = "TotalReturn"
+      ),
+      
+      selectInput(
+        inputId = "lookbackPeriod",
+        label = "Lookback Period:",
+        choices = choiceLookback,
+        selected = "MTD"
+      ),
+      
+      selectInput(
+        inputId = "theme",
+        label = "Theme",
+        choices = c(
+          "No theme",
+          "Chalk" = "chalk",
+          "Dark Unica" = "darkunica",
+          "Economist" = "economist",
+          "FiveThirtyEight" = "fivethirtyeight",
+          "Gridlight" = "gridlight",
+          "Handdrawn" = "handdrawn",
+          "Sandsignika" = "sandsignika"
+        ),
+        selected = "fivethirtyeight"
+      ),
+      br(),
+      h4("Table Parameters:"),
+      selectInput(
+        inputId = "TableDate",
+        label = "Pivot Table Date:",
+        choices = years,
+        selected = max(perf_data$Date)
+      ),
+      multiInput(
+        inputId = "listMeasures",
+        label = "List of Risk measures:",
+        choices = choiceMeasure,
+        selected = c("TotalReturn", "DownsideRisk", "Alpha")
+      )
+    )
+  )
 
 # SERVER ------------------------------------------------------------
 server = function(input, output) {
@@ -95,6 +119,45 @@ server = function(input, output) {
       filter(Measure == input$measureSelect) %>%
       filter(LookbackPeriod == input$lookbackPeriod) %>%
       filter(between(Date, input$dateRange[1], input$dateRange[2]))
+  })
+  pivotTblData <- reactive({
+    perf_data %>%
+      filter(Date == input$TableDate)
+  })
+  # Pivottabler -------------------------------------------------------
+  tblData <- reactive({
+    perf_data %>%
+      filter(Date == input$TableDate) %>%
+      filter(Measure %in% input$listMeasures)
+  })
+  output$pvt <- renderPivottabler({
+    pt <- PivotTable$new()
+    pt$addData(tblData())
+    pt$addColumnDataGroups(
+      "LookbackPeriod",
+      addTotal = FALSE,
+      dataSortOrder = "custom",
+      customSortOrder = c("MTD", "1M", "3M", "1Y", "3Y")
+    )
+    pt$addColumnDataGroups(
+      "Measure",
+      addTotal = FALSE,
+      dataSortOrder = "custom",
+      customSortOrder = choiceMeasure
+    )
+    pt$addRowDataGroups(
+      "Portfolio",
+      addTotal = FALSE,
+      dataSortOrder = "custom",
+      customSortOrder = c("Portfolio", "Benchmark", "ActiveReturn")
+    )
+    pt$defineCalculation(
+      calculationName = "value",
+      summariseExpression = "mean(value, na.rm=TRUE)",
+      format = "%.3f"
+    )
+    pt$evaluatePivot()
+    pivottabler(pt)
   })
   
   # Highchart -------------------------------------------------------
@@ -180,6 +243,10 @@ shiny::shinyApp(ui = ui, server = server)
 
 
 # Resources ------------------------------------------------------
-# shiny::runGitHub("learn-R", "QuantFinEcon", subdir = "shiny")
+# shiny::runGitHub("learn-R", "QuantFinEcon", subdir = "shiny-fundperformance")
 # shiny::runGitHub("shiny-examples", "rstudio", subdir = "118-highcharter-births")
 # https://byollin.github.io/RInteractiveCharts/#1
+# http://www2.stat.duke.edu/courses/Summer20/sta323.001-1/lecture_slides/lec-17.html#1
+# https://cran.r-project.org/web/packages/highcharter/highcharter.pdf
+# http://www.pivottabler.org.uk/articles/v14-shiny.html
+# http://pivottabler.org.uk/articles/v03-calculations.html
